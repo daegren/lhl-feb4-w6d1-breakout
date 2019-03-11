999ba677996afdc79dc750694a90140c8a9cd1ae
David Mills <dave@1morepx.com>
2019-03-11 14:52:44 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  devtool: 'eval',
+  mode: NODE_ENV,
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+};
